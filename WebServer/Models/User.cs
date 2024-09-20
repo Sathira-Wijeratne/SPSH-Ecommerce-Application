2a8a9b0464186b799710a827df8b268bcd433196
Sathira-Wijeratne <swije27@gmail.com>
2024-09-20 11:10:07 +0530

    chnaged model structure

--- WebServer/Models/User.cs
+++ WebServer/Models/User.cs
@@ -1,7 +1,12 @@
-﻿namespace SPSH_Ecommerce_Application.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SPSH_Ecommerce_Application.Models
 {
     public class User
     {
+        [BsonId] // Maps this property to the MongoDB _id field
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
         public string Role { get; set; }
