59c74e59ac449e45147bb4256a2ae9b49c4ab9fb
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-09-20 11:24:58 +0530

    Created cart model

---
+++ WebServer/Models/Cart.cs
@@ -0,0 +1,13 @@
+﻿namespace SPSH_Ecommerce_Application.Models
+{
+    public class Cart
+    {
+        public string Id { get; set; }
+        public string CustomerEmail { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string vendorEmail { get; set; }
+        public int ProductQty { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
