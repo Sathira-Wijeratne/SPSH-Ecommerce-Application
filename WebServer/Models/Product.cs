7770ebbd3a5438ba63e397bd495ab15275b5ad23
Sathira-Wijeratne <swije27@gmail.com>
2024-09-20 12:38:27 +0530

    finalized Product Model

--- WebServer/Models/Product.cs
+++ WebServer/Models/Product.cs
@@ -5,6 +5,8 @@ namespace SPSH_Ecommerce_Application.Models
     public class Product
     {
         public string Id { get; set; }
+        public string ProductId { get; set; }
+        public string VendorEmail { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
