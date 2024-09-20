7b1e35f71875cc5c69b58476fe608e01a3b6ff98
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-09-20 12:32:22 +0530

    Added Bson id for cart model

--- WebServer/Models/Cart.cs
+++ WebServer/Models/Cart.cs
@@ -1,7 +1,12 @@
-﻿namespace SPSH_Ecommerce_Application.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace SPSH_Ecommerce_Application.Models
 {
     public class Cart
     {
+        [BsonId] // Maps this property to the MongoDB _id field
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string CustomerEmail { get; set; }
         public string ProductId { get; set; }
