6957fe78e82c258f36e56fb19e93aa906e406a18
Sathira-Wijeratne <swije27@gmail.com>
2024-09-20 13:21:10 +0530

    created notification model

---
+++ WebServer/Models/CustomerNotfication.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SPSH_Ecommerce_Application.Models
+{
+    public class CustomerNotfication
+    {
+        [BsonId] // Maps this property to the MongoDB _id field
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string OrderId { get; set; }
+
+        public bool MarkAsRead { get; set; }
+        public string NotificationMessage { get; set; }
+    }
+}
