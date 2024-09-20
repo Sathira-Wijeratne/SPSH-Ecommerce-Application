326df885db069809185abf5644678f2c7100b0b0
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-09-20 12:49:07 +0530

    Implemented Rate model

---
+++ WebServer/Models/Rate.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace SPSH_Ecommerce_Application.Models
+{
+    public class Rate
+    {
+        [BsonId] // Maps this property to the MongoDB _id field
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string VendorEmail { get; set; }
+        public string CustomerEmail { get; set; }
+        public int Stars { get; set; }
+        public string Comment { get; set; }
+    }
+}
