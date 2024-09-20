41b6988a8e2e1e0093a4017b4825cbbc43dec416
Sathira-Wijeratne <swije27@gmail.com>
2024-09-20 13:21:21 +0530

    created category model

---
+++ WebServer/Models/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace SPSH_Ecommerce_Application.Models
+{
+    public class ProductCategory
+    {
+        [BsonId] // Maps this property to the MongoDB _id field
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductId { get; set; }
+    }
+}
