75782a085c515c6a956586c76852f8285d4c8de3
Hilarina <hilarinamelani21@gmail.com>
2024-09-22 22:17:58 +0530

    Unused import statement removed

--- MobileApplication/app/src/main/java/com/example/ead_mobile_app/CustomerLoginActivity.java
+++ MobileApplication/app/src/main/java/com/example/ead_mobile_app/CustomerLoginActivity.java
@@ -8,7 +8,6 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
