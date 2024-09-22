7581cbe51c50d994c64d07d114c24e52d1bc6287
SanuthiVihansa <subasofts@gmail.com>
2024-09-22 16:19:46 +0530

    fix the background image issue

--- WebApplication/frontend/src/Components/LoginForm/Login.jsx
+++ WebApplication/frontend/src/Components/LoginForm/Login.jsx
@@ -1,32 +1,52 @@
 import React from "react";
 import './Login.css';
-import { FaUser,FaLock} from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa";
+import backgroundImage from "../Assets/loginimage.jpg";
 
 
-const Login =()=>{
-    return(
-        <div className="Wrapper">
-            <form action="">
-                <h1>Login</h1>
-                {/* username */}
-                <div className="input-box">
-                    <input type="text" placeholder="Username" required/>
-                    <FaUser className="icon"/>
 
-                </div>
-                {/* password */}
-                <div className="input-box">
-                    <input type="password" placeholder="Password" required/>
-                    <FaLock className="icon"/>
-                </div>
-                <button type="submit">Login</button>
 
-                <div className="register-Link">
-                    {/* <p>Don't have an account? <a href="#">Register</a></p> */}
-                </div>
-            </form>
+const Login = () => {
 
+    const myStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+    };
+
+
+
+    return (
+        <div style={myStyle}>
+            <div className="Wrapper">
+                <form action="">
+                    <h1>Login</h1>
+                    {/* username */}
+                    <div className="input-box">
+                        <input type="text" placeholder="Username" required />
+                        <FaUser className="icon" />
+
+                    </div>
+                    {/* password */}
+                    <div className="input-box">
+                        <input type="password" placeholder="Password" required />
+                        <FaLock className="icon" />
+                    </div>
+                    <button type="submit">Login</button>
+
+                    <div className="register-Link">
+                        {/* <p>Don't have an account? <a href="#">Register</a></p> */}
+                    </div>
+                </form>
+            </div>
         </div>
+
     )
 }
 export default Login;
