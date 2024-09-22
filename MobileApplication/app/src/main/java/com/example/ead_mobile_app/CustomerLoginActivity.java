270053c18d7e6f9d5f52bc856e9dd1be34f2025f
Hilarina <hilarinamelani21@gmail.com>
2024-09-22 22:16:59 +0530

    Customer Login Implementation

--- MobileApplication/app/src/main/java/com/example/ead_mobile_app/CustomerLoginActivity.java
+++ MobileApplication/app/src/main/java/com/example/ead_mobile_app/CustomerLoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.ead_mobile_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,44 @@ public class CustomerLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.customer_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        EditText emailEditText = findViewById(R.id.loginEmailEditText);
+        EditText passwordEditText = findViewById(R.id.loginPasswordEditText);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(v -> {
+            String email = emailEditText.getText().toString().trim();
+            String password = passwordEditText.getText().toString().trim();
+
+            if(validateInputs(email,password)){
+                loginUser(email,password);
+            }else{
+                Toast.makeText(CustomerLoginActivity.this,"Invalid Credentials",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean validateInputs(String email, String password){
+        return !email.isEmpty() && !password.isEmpty();
+    }
+
+    private void loginUser(String email, String password){
+        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupUiForImmersiveEdgeToEdge(View view) {
+        ViewCompat.setOnApplyWindowInsetsListener(view, (v, insets) -> {
+            v.setPadding(
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars()).left,
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars()).top,
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars()).right,
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom
+            );
+            return WindowInsetsCompat.CONSUMED;
         });
     }
 }
