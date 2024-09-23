1ff0e1c49692cdd483d2e319f00e4b069f54aa6a
SanuthiVihansa <subasofts@gmail.com>
2024-09-23 23:20:19 +0530

    import routes

--- WebApplication/frontend/src/App.js
+++ WebApplication/frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import './index.css';
 import Login from './Components/loginScreen/Login';
-import AdminDashbord from './Components/adminScreens/AdminDashboardScreen';
+import AdminDashbord from './Components/adminScreens/adminDashboard/AdminDashboardScreen';
 
 
 function App() {
