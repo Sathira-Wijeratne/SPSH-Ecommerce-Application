f3a89268683551f5857a358f00abdf1addaac6c2
SanuthiVihansa <subasofts@gmail.com>
2024-09-23 21:24:52 +0530

    menubar completed

--- WebApplication/frontend/src/Components/adminScreens/adminDashboard/menuBar/MenuBar.jsx
+++ WebApplication/frontend/src/Components/adminScreens/adminDashboard/menuBar/MenuBar.jsx
@@ -1,9 +1,31 @@
+import React from "react";
+import { HiShoppingCart } from "react-icons/hi2";
+
+import './MenuBar.css';
+
 const MenuBar = () => {
 
     return (
 
-        <div>
-            Hi
+        <div className="menu">
+            <div className="logo">
+                <HiShoppingCart className="logo-icon" />
+                    <h2>SPSH</h2>
+            </div>
+            <div className="menulist">
+
+                <a href="#" className="item">Dashboard</a>
+
+                <a href="#" className="item">User Management</a>
+
+                <a href="#" className="item">Vendor Management</a>
+
+                <a href="#" className="item">Product Management</a>
+
+                <a href="#" className="item">Order Management</a>
+
+                <a href="#" className="item">CSR Management</a>
+            </div>
         </div>
 
 
