c8bde1654c4f23988fe60f567860ee558a52771a
SanuthiVihansa <subasofts@gmail.com>
2024-09-23 23:22:07 +0530

    DashboardContent.css

--- WebApplication/frontend/src/Components/adminScreens/adminDashboard/dashboardContent/DashboardContent.jsx
+++ WebApplication/frontend/src/Components/adminScreens/adminDashboard/dashboardContent/DashboardContent.jsx
@@ -1,12 +1,21 @@
+import React from "react";
+import { BiNotification } from 'react-icons/bi';
+import './DashboardContent.css'
+
 const DashboardContent = () => {
 
     return (
 
-        <div className="">
-           Dashboard
-        </div>
-
-
-    )
-}
+        <div className="content--header">
+            <h1 className="header-title">Dashboard</h1>
+            <div className="header--activity">
+                <div className="profile">
+                </div>
+                <div className="notify">
+                    <BiNotification />
+                </div>
+            </div>
+            </div>
+    );
+};
 export default DashboardContent;
