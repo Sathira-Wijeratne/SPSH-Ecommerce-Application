bd41c3222b8152a0e1b595640632a22d96797a6a
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-09-25 18:51:38 +0530

    Fixed path issues

--- WebApplication/frontend/src/App.js
+++ WebApplication/frontend/src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import './index.css';
-import Login from './Components/loginScreen/Login';
-import AdminDashbord from './Components/adminScreens/adminDashboard/AdminDashboardScreen';
-
+import "./index.css";
+import Login from "./Components/LoginScreen/Login";
+import AdminDashbord from "./Components/adminScreens/adminDashboard/AdminDashboardScreen";
 
 function App() {
-  return(
+  return (
     <Router>
       <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/Admin/AdminDashboard" element={<AdminDashbord />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Admin/AdminDashboard" element={<AdminDashbord />} />
       </Routes>
     </Router>
   );
-  
 }
 
 export default App;
