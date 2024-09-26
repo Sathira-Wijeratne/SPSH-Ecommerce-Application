e3f2316757e8a850b86f0dfda843ced25abcfe31
SanuthiVihansa <subasofts@gmail.com>
2024-09-26 22:37:05 +0530

    vendor Menu Bar .jsx and css

---
+++ WebApplication/frontend/src/Components/vendorScreens/vendorMenuBar/VendorMenuBar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { HiShoppingCart } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+import './VendorMenuBar.css';
+
+const VendorMenuBar = () => {
+    const [showSubMenu, setShowSubMenu] = useState(false);
+    const [showSubForVendorMenu, setshowSubForVendorMenu] = useState(false);
+    const navigate = useNavigate(); // Initialize the useNavigate hook
+
+    const toggleSubMenu = () => {
+        setShowSubMenu(!showSubMenu);
+    };
+
+    const toggleSubMenuForVendor = () => {
+        setshowSubForVendorMenu(!showSubForVendorMenu);
+    };
+
+
+    // Function to navigate to the New User page
+    const onClickNewUser = (e) => {
+        e.preventDefault();
+        navigate("/Admin/UserManagement/NewUser");
+    };
+
+     // Function to navigate to the Dashboard
+     const onClickDashboard = (e) => {
+        e.preventDefault();
+        navigate("/Admin/AdminDashboard");
+    };
+
+
+
+
+    return (
+        <div className="menu">
+            <div className="logo">
+                <HiShoppingCart className="logo-icon" />
+                <h2>SPSH</h2>
+            </div>
+
+            <div className="menulist">
+                <a href="#" className="item" onClick={onClickDashboard}>Dashboard</a>
+
+                <div className="item" onClick={toggleSubMenu}>
+                    Orders
+                </div>
+        
+                  <div className="item" onClick={toggleSubMenuForVendor}>
+                    Shipment Management
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default VendorMenuBar;
