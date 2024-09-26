cd1d9d983eeeb5a3bf44562571ef60ee16b1f50f
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-09-26 10:55:10 +0530

    Implemented login function

--- WebApplication/frontend/src/Components/LoginScreen/Login.jsx
+++ WebApplication/frontend/src/Components/LoginScreen/Login.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 import { FaUser, FaLock } from "react-icons/fa";
-import backgroundImage from "../assets/loginimage.jpg";
+import backgroundImage from "../Assets/loginimage.jpg";
+import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [userDetails, setUserDetails] = useState({});
 
   //Background Image related inline css
   const myStyle = {
@@ -24,7 +29,31 @@ const Login = () => {
   //Function to navigate to Admin Dashboard Screen
   const onClickLogin = (e) => {
     e.preventDefault();
-    navigate("/Admin/AdminDashboard");
+    axios
+      .get(`http://192.168.137.1:2030/api/Users/${username}`)
+      .then((res) => {
+        console.log(res.data);
+        setUserDetails(res.data);
+        if (res.data.password === password) {
+          if (res.data.role === "Admin") {
+            sessionStorage.setItem("email", res.data.email);
+            navigate("/Admin/AdminDashboard");
+          } else if (res.data.role === "CSR") {
+            sessionStorage.setItem("email", res.data.email);
+            navigate("/CSR/CSRDashboard");
+          } else if (res.data.role === "Vendor") {
+            sessionStorage.setItem("email", res.data.email);
+            navigate("/Vendor/VendorDashboard");
+          } else {
+            alert("Unauthorized User!");
+          }
+        } else {
+          alert("Invalid Credentials!");
+        }
+      })
+      .catch((err) => {
+        alert("Invalid Credentials!");
+      });
   };
 
   return (
@@ -34,12 +63,26 @@ const Login = () => {
           <h1>Login</h1>
           {/* username */}
           <div className="input-box">
-            <input type="text" placeholder="Username" required />
+            <input
+              type="text"
+              placeholder="Username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              required
+            />
             <FaUser className="icon" />
           </div>
           {/* password */}
           <div className="input-box">
-            <input type="password" placeholder="Password" required />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
             <FaLock className="icon" />
           </div>
           <button type="submit" onClick={onClickLogin}>
