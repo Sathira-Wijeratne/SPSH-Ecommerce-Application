b866272f06f0527e584fb9087dd3c0e47ce18b6e
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-09-27 14:24:27 +0530

    Implemented displaying recent orders

--- WebApplication/frontend/src/Components/vendorScreens/vendorDashboard/VendorDashboard.jsx
+++ WebApplication/frontend/src/Components/vendorScreens/vendorDashboard/VendorDashboard.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import MenuBar from '../vendorMenuBar/VendorMenuBar'; 
-import Content from './VendorDashboardContent';
-import Card from './VendorDashbordCard';
-import './VendorDashboard.css';
+import React, { useEffect, useState } from "react";
+import MenuBar from "../vendorMenuBar/VendorMenuBar";
+import Content from "./VendorDashboardContent";
+import Card from "./VendorDashbordCard";
+import "./VendorDashboard.css";
+import axios from "axios";
 
 const VendorDashboard = () => {
+  const vendorEmail = sessionStorage.getItem("email");
+  const [recentOrders, setRecentOrders] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://192.168.137.1:2030/api/Orders/vendor-recent-orders/${vendorEmail}`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setRecentOrders(res.data);
+      })
+      .catch((err) => {});
+  }, []);
+
   return (
     <div className="content">
       <div className="dashboard">
@@ -12,48 +28,49 @@ const VendorDashboard = () => {
         <div className="dashboard--content">
           <Content />
           <Card />
-          
+
           {/* Adding the Table below the cards */}
           <div className="recent-orders">
             <h3>Recent Orders</h3>
-            <table className="orders-table">
-              <thead>
-                <tr>
-                  <th>Product</th>
-                  <th>Status</th>
-                  <th>Price</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>boAt 220 Wireless</td>
-                  <td><span className="pending">Pending</span></td>
-                  <td>₹1,399</td>
-                </tr>
-                <tr>
-                  <td>Fitbit Inspire</td>
-                  <td><span className="delivered">Delivered</span></td>
-                  <td>₹4,499</td>
-                </tr>
-                <tr>
-                  <td>Solid Men Polo Neck Blue T-Shirt</td>
-                  <td><span className="delivered">Delivered</span></td>
-                  <td>₹1,174</td>
-                </tr>
-                <tr>
-                  <td>Fitbit Inspire</td>
-                  <td><span className="pending">Pending</span></td>
-                  <td>₹1,399</td>
-                </tr>
-                <tr>
-                  <td>Mirrored Aviator Sunglasses</td>
-                  <td><span className="delivered">Delivered</span></td>
-                  <td>₹6,831</td>
-                </tr>
-              </tbody>
-            </table>
+            {recentOrders.length > 0 ? (
+              <table className="orders-table">
+                <thead>
+                  <tr>
+                    <th>Product</th>
+                    <th>Status</th>
+                    {/* <th>Quantity</th> */}
+                    <th>Unit Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {recentOrders.map((order) => (
+                    <tr>
+                      <td>{order.productName}</td>
+                      <td>
+                        {order.status === "Processing" ? (
+                          <span className="pending">{order.status}</span>
+                        ) : order.status === "Delivered" ? (
+                          <span className="delivered">{order.status}</span>
+                        ) : order.status === "Completed" ? (
+                          <span className="delivered">{order.status}</span>
+                        ) : order.status === "Cancelled" ? (
+                          <span className="cancelled">{order.status}</span>
+                        ) : order.status === "Requested to cancel" ? (
+                          <span className="cancelled">{order.status}</span>
+                        ) : (
+                          <></>
+                        )}
+                      </td>
+                      {/* <td>{order.productQuantity}</td> */}
+                      <td>Rs. {order.productUnitPrice.toFixed(2)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <center>No Recent Orders</center>
+            )}
           </div>
-          
         </div>
       </div>
     </div>
