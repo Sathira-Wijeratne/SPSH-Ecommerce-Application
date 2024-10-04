bfcccaed8ddc8acc0dca4cb770d9225c53701a48
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-10-04 15:23:10 +0530

    Implemented logics for vendor dashboard cards

--- WebApplication/frontend/src/Components/vendorScreens/vendorDashboard/VendorDashbordCard.jsx
+++ WebApplication/frontend/src/Components/vendorScreens/vendorDashboard/VendorDashbordCard.jsx
@@ -1,37 +1,90 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { FaTruck, FaSpinner, FaDollarSign } from "react-icons/fa"; // You can use these icons or choose others
 
-// Updated array with new titles and icons
-const stockInfo = [
-    {
-        title: 'Sold Stocks',
-        icon: <FaTruck />, // Icon representing delivered stocks
-    },
-    {
-        title: 'Order Pending',
-        icon: <FaSpinner />, // Icon representing processing stocks
-    },
-    {
-        title: 'Total Sales',
-        icon: <FaDollarSign />, // Icon representing total sales
-    },
-];
-
 const VendorDashboardCard = () => {
-    return (
-        <div className="card-container">
-            {stockInfo.map((item, index) => (
-                <div className="card" key={index}>
-                    <div className="card-cover">
-                        {item.icon}
-                    </div>
-                    <div className="card-title">
-                        <h2>{item.title}</h2>
-                    </div>
-                </div>
-            ))}
+  // States to hold order and sales data
+  const [pendingOrders, setPendingOrders] = useState(0);
+  const [deliveredStocks, setDeliveredStocks] = useState(0);
+  const [completedStocks, setCompletedStocks] = useState(0);
+  const [deliveredSales, setDeliveredSales] = useState(0);
+  const [completedSales, setCompletedSales] = useState(0);
+
+  const vendorEmail = sessionStorage.getItem("email");
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://192.168.137.1:2030/api/Orders/get-by-vendor-status/${vendorEmail}/Delivered`
+      )
+      .then((res) => {
+        var soldCount = 0;
+        var sales = 0;
+        for (var i = 0; i < res.data.length; i++) {
+          soldCount = soldCount + res.data[i].productQuantity;
+          sales =
+            sales + res.data[i].productUnitPrice * res.data[i].productQuantity;
+        }
+        setDeliveredStocks(soldCount);
+        setDeliveredSales(sales);
+      });
+
+    axios
+      .get(
+        `http://192.168.137.1:2030/api/Orders/get-by-vendor-status/${vendorEmail}/Completed`
+      )
+      .then((res) => {
+        var soldCount = 0;
+        var sales = 0;
+        for (var i = 0; i < res.data.length; i++) {
+          soldCount = soldCount + res.data[i].productQuantity;
+          sales =
+            sales + res.data[i].productUnitPrice * res.data[i].productQuantity;
+        }
+        setCompletedStocks(soldCount);
+        setCompletedSales(sales);
+      });
+
+    axios
+      .get(
+        `http://192.168.137.1:2030/api/Orders/get-by-vendor-status/${vendorEmail}/Processing`
+      )
+      .then((res) => {
+        setPendingOrders(res.data.length);
+      });
+  }, []);
+
+  return (
+    <div className="card-container">
+      <div className="card">
+        <div className="card-cover">
+          <FaTruck />
         </div>
-    );
-}
+        <div className="card-title">
+          <h2>Sold Stocks</h2>
+          <p>{deliveredStocks + completedStocks}</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-cover">
+          <FaSpinner />
+        </div>
+        <div className="card-title">
+          <h2>Order Pending</h2>
+          <p>{pendingOrders}</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-cover">
+          <FaDollarSign />
+        </div>
+        <div className="card-title">
+          <h2>Total Sales</h2>
+          <p>Rs. {deliveredSales + completedSales}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default VendorDashboardCard;
