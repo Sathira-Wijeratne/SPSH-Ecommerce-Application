91e7c26e725eba224e709bed8f3ca23faf651df2
SanuthiVihansa <subasofts@gmail.com>
2024-10-06 20:48:51 +0530

    confirm alert stock remove

--- WebApplication/frontend/src/Components/adminScreens/inventoryManagement/InventoryManagement.jsx
+++ WebApplication/frontend/src/Components/adminScreens/inventoryManagement/InventoryManagement.jsx
@@ -7,45 +7,52 @@ import MenuBar from "../adminDashboard/menuBar/MenuBar";
 const InventoryManagement = () => {
   const [stock, setStock] = useState([]);
   const [searchTerm, setSearchTerm] = useState(""); // Search term state
-  const vendorEmail = sessionStorage.getItem("email");
+  const [showDialog, setShowDialog] = useState(false);  // To toggle the dialog
+  const [selectedProductId, setSelectedProductId] = useState(null); // To store selected product ID
 
   useEffect(() => {
-    axios.get(`http://192.168.137.1:2030/api/Products `).then((res) => {
+    axios.get(`http://192.168.137.1:2030/api/Products`).then((res) => {
       console.log(res.data);
       setStock(res.data);
     });
   }, []);
 
-  const handleRemoveStock = (productId) => {
-    var userRes = window.confirm(
-      `Are you sure you want to remove all the stocks of product ${productId}?`
-    );
-    if (userRes === true) {
-      axios
-        .get(
-          `http://192.168.137.1:2030/api/Orders/get-by-status-prodid/Processing/${productId}`
-        )
-        .then((res) => {
-          if (res.data.length > 0) {
-            alert("Cannot remove stocks due to some processing orders!");
-          } else {
-            axios
-              .delete(`http://192.168.137.1:2030/api/Products/${productId}`)
-              .then((res) => {
-                if (res.status === 200) {
-                  alert("Stocks Removed!");
-                  window.location.reload();
-                }
-              })
-              .catch((err) => {
-                alert(err);
-              });
-          }
-        })
-        .catch((err) => {
-          alert(err);
-        });
-    }
+  // Function to show dialog and store selected product ID
+  const confirmRemoveStock = (productId) => {
+    setSelectedProductId(productId);
+    setShowDialog(true); // Show the dialog
+  };
+
+  // Function to remove stock
+  const handleRemoveStock = () => {
+    axios
+      .get(
+        `http://192.168.137.1:2030/api/Orders/get-by-status-prodid/Processing/${selectedProductId}`
+      )
+      .then((res) => {
+        if (res.data.length > 0) {
+          alert("Cannot remove stocks due to some processing orders!");
+          setShowDialog(false); // Hide dialog if there are processing orders
+        } else {
+          axios
+            .delete(`http://192.168.137.1:2030/api/Products/${selectedProductId}`)
+            .then((res) => {
+              if (res.status === 200) {
+                alert("Stocks Removed!");
+                setShowDialog(false); // Hide dialog after deletion
+                window.location.reload();
+              }
+            })
+            .catch((err) => {
+              alert(err);
+              setShowDialog(false); // Hide dialog if there is an error
+            });
+        }
+      })
+      .catch((err) => {
+        alert(err);
+        setShowDialog(false); // Hide dialog if there is an error
+      });
   };
 
   // Function to filter stock based on search term
@@ -96,16 +103,40 @@ const InventoryManagement = () => {
                 <td>
                   <Button
                     variant="danger" // Bootstrap variant for red button
-                    onClick={() => handleRemoveStock(product.productId)}
+                    onClick={() => confirmRemoveStock(product.productId)} // Show dialog when remove is clicked
                     className="remove-btn"
                   >
-                    Remove Stock
+                    Remove
                   </Button>
                 </td>
               </tr>
             ))}
           </tbody>
         </Table>
+
+        {/* Confirmation Dialog Box */}
+        {showDialog && (
+          <div className="dialog-overlay">
+            <div className="dialog-box">
+              <h3>Confirm Deletion</h3>
+              <p>
+                Are you sure you want to Remove all the stocks of product{" "}
+                <strong>{selectedProductId}</strong>?
+              </p>
+              <div className="dialog-buttons">
+                <Button variant="danger" onClick={handleRemoveStock}>
+                  Yes, Remove
+                </Button>
+                <Button
+                  variant="secondary"
+                  onClick={() => setShowDialog(false)} // Hide dialog when cancel is clicked
+                >
+                  Cancel
+                </Button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
